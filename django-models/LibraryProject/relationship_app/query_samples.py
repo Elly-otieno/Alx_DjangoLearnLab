604fdc90e5ebb18e1d2426b46885584e4379aeea
Elly-otieno <okothelli@gmail.com>
2025-11-14 15:45:24 +0300

    relationship models modified

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -11,11 +11,13 @@ Retrieve the librarian for a library.
 
 # Query all books by a specific author.
 
+
 author = Author.objects.get(name='Elly Okoth')
 books = author.books.all()
 
 # List all books in a library.
-library = Library.objects.get(name='Technology')
+library_name = 'Technology'
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
 # Retrieve the librarian for a library.
