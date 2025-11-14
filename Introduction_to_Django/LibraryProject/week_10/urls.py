83b78d22b0bfd9f5b03c4ac1a02911fd568f5fb8
Elly-otieno <okothelli@gmail.com>
2025-11-14 18:48:52 +0300

    adding tasks changes

--- Introduction_to_Django/LibraryProject/week_10/urls.py
+++ Introduction_to_Django/LibraryProject/week_10/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello_view, name='hello'), # function based view
-    path('about/', view.AboutView.as-view(). name='about'), # class based view
+    path('hello/', views.hello_view, name='hello'), 
+    path('about/', views.AboutView.as_view(), name='about'), 
 ]
