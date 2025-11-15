9c2b884e627fe48bdfa84d4ce3946ca7a0995da7
Elly-otieno <okothelli@gmail.com>
2025-11-15 11:26:13 +0300

    changes on templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, SignUpView
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, SignUpView, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('books/', list_books, name='books'),
     path('library-details/', LibraryDetailView.as_view(), name='library-details'),
     path('register/', SignUpView.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 
 # from django.views.generic import DetailView
 from django.views.generic.detail import DetailView, CreateView
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
 
@@ -23,3 +24,11 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+
+class CustomLoginView(LoginView):
+    template_name= 'relationship_app/login.html'
+
+
+class CustomLogoutView(LogoutView):
+    template_name= 'relationship_app/logout.html'
