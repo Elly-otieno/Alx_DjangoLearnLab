a147d1a411d9ce85b27e9c8ef958cbf4e63b56e6
Elly-otieno <okothelli@gmail.com>
2025-12-03 17:24:37 +0300

    viewsets

--- api_project/week_12/urls.py
+++ api_project/week_12/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
-from .views import BookListCreateAPIView
+from django.urls import path, include
+from .views import BookListCreateAPIView, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+'''
+DefaultRouter: Creates standard API root view and generates URLs for ViewSet actions.
+SimpleRouter: Similar to DefaultRouter but without the API root view, suitable for simpler APIs.
+Custom Routers: Allows you to define custom routing logic for more complex API structures.
+'''
+router = DefaultRouter()
+router.register(r'my-models', BookViewSet)
 
 urlpatterns = [
-    path("api/books", BookListCreateAPIView.as_view(), name="book_list_create")
+    path("api/books", BookListCreateAPIView.as_view(), name="book_list_create"),
+    path("api/", include(router.urls))
 ]
--- api_project/week_12/views.py
+++ api_project/week_12/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -16,3 +16,18 @@ class BookListCreateAPIView(generics.ListCreateAPIView):
         if title_filter is not None:
             queryset = queryset.filter(title__icontains=title_filter)  
         return queryset
+
+
+'''
+VIEWSETS - 
+Provides high level abstraction for creating API views that handle common CRUD operations on models
+ViewSets group these actions (e.g., list, retrieve, create, update, delete) together, reducing code duplication and promoting consistency.
+
+ModelViewSet: Provides a complete set of CRUD operations for a model, including list, retrieve, create, update, and delete actions.
+ReadOnlyModelViewSet: Offers read-only operations, such as list and retrieve, suitable for exposing data without allowing modifications.
+ViewSet: A base class that allows you to define custom actions and implement specific API behavior.
+'''
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
