20b8c63d49f78c05700359d55c9bf8ea3adf1cbe
Elly-otieno <okothelli@gmail.com>
2025-12-03 20:52:17 +0300

    folder restructure

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Book, Author
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'publication_year', 'author']
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name']
