c538733d0fe72494a2f261012aafdc5ee389b007
Elly-otieno <okothelli@gmail.com>
2025-12-07 21:11:15 +0300

    add comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -7,3 +7,17 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return f"Post by {self.author.username} created at {self.published_date}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
