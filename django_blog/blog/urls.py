e18e58ab5106f82953358bd57ae58d0bf5fbc42a
Elly-otieno <okothelli@gmail.com>
2025-12-24 21:32:06 +0300

    feat: user signup form

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views   # django in built auth 
-from .views import SignUpView
+from .views import SignUpView, PostCreateView, PostDeleteView, PostDetailView, PostDisplayView, PostUpdateView
 
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html', name='login')),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html', name='logout')),
-    path('register/', SignUpView.as_view(template_name='blog/register.html', name='register'))
+    path('register/', SignUpView.as_view(template_name='blog/register.html', name='register')),
+    path('posts/', PostDisplayView.as_view(template_name='blog/posts.html', name='posts')),
+    path('posts/new/', PostCreateView.as_view(template_name='blog/create_post.html', name='create_post')),
+    path('posts/<int:pk>/', PostDetailView.as_view(template_name='blog/view_post.html', name='view_post')),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(template_name='blog/edit_post.html', name='edit_post')),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(template_name='blog/delete_post.html', name='delete_post')),
 ]
